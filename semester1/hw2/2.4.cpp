82cb0c2cbc23a06a33b4662b8046f3840bdd051d
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2018-10-08 19:23:48 +0300

    Не знаю что произошло с прошлой версией, но с ней что то произошло

--- semester1/hw2/2.4.cpp
+++ semester1/hw2/2.4.cpp
@@ -1,12 +1,12 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include pch.h
-#include stdio.h
-#include stdlib.h
-#include time.h 
+#include "pch.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h> 
 
 void fillArray(int data[], const int size);
 void swap(int &first, int &second);
-void printArray(int data[],const int size);
+void printArray(int data[], const int size);
 void doingWork(int data[], const int size);
 bool tests();
 
@@ -14,14 +14,14 @@ int main()
 {
 	if (!tests())
 	{
-		printf(nSomething went wrong !!n);
+		printf("\nSomething went wrong !!\n");
 		return 1;
 	}
 	srand(time(NULL));
 	int size = 0;
-	printf(Please enter array size );
-	scanf(%i, &size);
-	int data = new int[size] {};
+	printf("Please enter array size");
+	scanf("%i", &size);
+	int *data = new int[size] {};
 	fillArray(data, size);
 	printArray(data, size);
 	doingWork(data, size);
@@ -31,7 +31,7 @@ int main()
 
 void fillArray(int data[], const int size)
 {
-	for (int i = 0; i  size; i++)
+	for (int i = 0; i < size; i++)
 	{
 		data[i] = rand();
 	}
@@ -46,11 +46,11 @@ void swap(int & first, int & second)
 
 void printArray(int data[], const int size)
 {
-	for (int i = 0; i  size; i++)
+	for (int i = 0; i < size; i++)
 	{
-		printf(%i , data[i]);
+		printf("%i ", data[i]);
 	}
-	printf(n n);
+	printf("\n \n");
 }
 
 void doingWork(int data[], const int size)
@@ -60,11 +60,11 @@ void doingWork(int data[], const int size)
 	int ptrLeft = 0;
 	while (ptrRight != ptrLeft)
 	{
-		while (data[ptrLeft]  number && ptrRight  ptrLeft)
+		while (data[ptrLeft] < number && ptrRight > ptrLeft)
 		{
 			ptrLeft++;
 		}
-		while (data[ptrRight] = number && ptrRight  ptrLeft)
+		while (data[ptrRight] >= number && ptrRight > ptrLeft)
 		{
 			ptrRight--;
 		}
@@ -75,18 +75,18 @@ bool tests()
 {
 	srand(time(NULL));
 	const int SIZE = 10;
-	for (int i = 0; i  10; i++)
+	for (int i = 0; i < 10; i++)
 	{
 		int data[SIZE];
 		fillArray(data, SIZE);
 		int number = data[0];
 		doingWork(data, SIZE);
 		i = 0;
-		while (data[i]  number)
+		while (data[i] < number)
 		{
 			i++;
 		}
-		while (data[i] = number && i  SIZE)
+		while (data[i] >= number && i < SIZE)
 		{
 			i++;
 		}
