4a4db77a4bb847dc7f4d431313ae9ad76532a811
vologin-dmitry <etapochradlyavas@gmail.com>
2018-11-08 17:39:43 +0300

    Включил отображение, вроде только пробелы. Но возможно это автоматически поставилось

--- semester1/hw2/2.1.cpp
+++ semester1/hw2/2.1.cpp
@@ -1,5 +1,5 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include "pch.h"
+//#include "pch.h"
 #include <stdio.h>
 
 int recursiveFibonacci(const int n);
@@ -10,52 +10,52 @@ bool test();
 
 int main()
 {
-	printf("Please wait, tests passing\n");
-	if (!test())
-	{
-		printf("Tests passed successfully !\n<--------------------------------------------------------------->\n");
-	}
-	else
-	{
-		printf("ERROR! Something went wrong\n");
-		return 1;
-	}
-	int choose = ' ';
-	printf("Input 1 to choose recursive algorithm\nInput 2 to choose iterative algorithm ");
-	scanf("%c", &choose);
-	if (choose == '2')
-	{
-		iterativeFibonacciHelper();
-	}
-	if (choose == '1')
-	{
-		recursiveFibonacciHelper();
-	}
-	return 0;
+    printf("Please wait, tests passing\n");
+    if (!test())
+    {
+        printf("Tests passed successfully !\n<--------------------------------------------------------------->\n");
+    }
+    else
+    {
+        printf("ERROR! Something went wrong\n");
+        return 1;
+    }
+    int choose = ' ';
+    printf("Input 1 to choose recursive algorithm\nInput 2 to choose iterative algorithm ");
+    scanf("%c", &choose);
+    if (choose == '2')
+    {
+        iterativeFibonacciHelper();
+    }
+    if (choose == '1')
+    {
+        recursiveFibonacciHelper();
+    }
+    return 0;
 }
 
 int recursiveFibonacci(const int n)
 {
-	if (n == 0)
-	{
-		printf("ERROR! Input valid number");
-		return 0;
-	}
-	if (n < 0)
-	{
-		printf("ERROR! Input valid number");
-		return -1;
-	}
-	if (n < 2)
-	{
-		return 1;
-	}
-	return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+    if (n == 0)
+    {
+        printf("ERROR! Input valid number");
+        return 0;
+    }
+    if (n < 0)
+    {
+        printf("ERROR! Input valid number");
+        return -1;
+    }
+    if (n < 2)
+    {
+        return 1;
+    }
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
 }
 
 int iterativeFibonacci(const int number)
 {
-    int temp[3]{0,0,1};
+    int temp[3]{ 0, 0, 1 };
     if (number < 0)
     {
         temp[2] = -1;
@@ -66,24 +66,24 @@ int iterativeFibonacci(const int number)
         temp[2] = 0;
         return temp[2];
     }
-	for (int i = 1;; i++)
-	{
+    for (int i = 1;; i++)
+    {
         if (i == number)
         {
             return temp[2];
         }
-		temp[0] = temp[1];
-		temp[1] = temp[2];
-		temp[2] = temp[0] + temp[1];
-	}
+        temp[0] = temp[1];
+        temp[1] = temp[2];
+        temp[2] = temp[0] + temp[1];
+    }
 }
 
 void recursiveFibonacciHelper()
 {
-	for (int i = 0;; i++)
-	{
-		printf("%i ", recursiveFibonacci(i));
-	}
+    for (int i = 0;; i++)
+    {
+        printf("%i ", recursiveFibonacci(i));
+    }
 }
 
 void iterativeFibonacciHelper(const int tooMuch)
@@ -96,19 +96,19 @@ void iterativeFibonacciHelper(const int tooMuch)
 
 bool test()
 {
-	int datas[]{ -1, 0, 1, 5, -5, 14 };
-	int answers[]{ -1, 0, 1, 5, -1, 377 };
-	for (int i = 0; i < 6; i++)
-	{
-		if (recursiveFibonacci(datas[i]) != answers[i])
-		{
-			return true;
-		}
-		if (iterativeFibonacci(datas[i]) != answers[i])
-		{
-			return true;
-		}
-	}
-	printf("\n\n");
-	return false;
+    int datas[]{ -1, 0, 1, 5, -5, 14 };
+    int answers[]{ -1, 0, 1, 5, -1, 377 };
+    for (int i = 0; i < 6; i++)
+    {
+        if (recursiveFibonacci(datas[i]) != answers[i])
+        {
+            return true;
+        }
+        if (iterativeFibonacci(datas[i]) != answers[i])
+        {
+            return true;
+        }
+    }
+    printf("\n\n");
+    return false;
 }
