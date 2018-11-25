e27820a49acdadd7d6c12c719583b10d4244783a
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2018-11-25 03:41:44 +0300

    Добавил комментарии

--- semester1/hw6/6.1/Stack.h
+++ semester1/hw6/6.1/Stack.h
@@ -11,7 +11,14 @@ struct Stack
 	StackElement *head = nullptr;
 };
 
+//Добавить элемент в стэк
 void push(Stack &stack, int data);
+
+//Вынуть элемент из стжка
 int pop(Stack &stack, bool &result);
+
+//Проверить является ли стэк пустным
 bool isEmpty(Stack stack);
+
+//Удалить стэк
 void deleteStack(Stack &stack);
