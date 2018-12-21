f8c2e781baaf3afc25eb1889c840bc3c68be409c
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2018-12-21 14:59:32 +0300

    3-2

---
+++ semester1/3-2/3-2/3-2.cpp
@@ -0,0 +1,49 @@
+﻿#include "pch.h"
+#include <iostream>
+#include "PriorityQueue.h"
+
+using namespace std;
+
+bool tests();
+
+int main()
+{
+	if (tests())
+	{
+		cout << "Something went wrong !!";
+		return 1;
+	}
+	PriorityQueue *queue = new PriorityQueue;
+	enqueue(queue, 5, "hello");
+	enqueue(queue, 1, "ewt");
+	enqueue(queue, 10, "wafiafo");
+	cout << dequeue(queue) << endl;
+	cout << dequeue(queue) << endl;
+	clear(queue);
+	delete queue;
+}
+
+bool tests()
+{
+	const int SIZE = 5;
+	PriorityQueue *queue = new PriorityQueue;
+	int priors[SIZE]{ 5, 3, 15, 15, 8 };
+	string datas[SIZE]{ "a","fa","awf","nevermind", "qu" };
+	string answers[SIZE]{ "awf", "nevermind", "qu", "a", "fa" };
+	for (int i = 0; i < SIZE; ++i)
+	{
+		enqueue(queue, priors[i], datas[i]);
+	}
+	for (int i = 0; i < SIZE; ++i)
+	{
+		if (dequeue(queue) != answers[i])
+		{
+			clear(queue);
+			delete queue;
+			return 1;
+		}
+	}
+	clear(queue);
+	delete queue;
+	return 0;
+}
