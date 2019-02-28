52197673d69b7b5616179fcee074cb34eb36903e
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2019-02-28 13:28:27 +0300

    Задание 2

---
+++ semester2/2.2/2.2/HashTable.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ namespace Table
+{
+    class HashTable
+    {
+        const int MAX = 100;
+        LinkedList.List[] MyTable = new LinkedList.List[MAX];
+
+         public HashTable()
+        {
+            for (int i = 0; i < MAX; ++i)
+            {
+                MyTable[i] = new LinkedList.List();
+            }
+        }
+
+         private int HashCode(string data)
+        {
+            return Math.Abs(data.GetHashCode()) % MAX;
+        }
+
+         public void Add(string data)
+        {
+            int hash = HashCode(data);
+            MyTable[hash].Add(data, MyTable[hash].GetSize() + 1);
+        }
+
+         public bool Delete(string data)
+        {
+            int hash = HashCode(data);
+            int position = GetPosition(data, hash);
+            if (position == -1)
+            {
+                return false;
+            }
+            MyTable[hash].Delete(position);
+            return true;
+        }
+
+         public bool Exists(string data)
+        {
+            return (GetPosition(data, HashCode(data)) != -1);
+        }
+
+         public void Clear()
+        {
+            for (int i = 0; i < MAX; ++i)
+            {
+                MyTable[i] = null;
+            }
+        }
+
+         private int GetPosition(string data, int hash)
+        {
+            int size = MyTable[hash].GetSize();
+            for (int i = 1; i <= size; ++i)
+            {
+                if (MyTable[hash].GetDataOn(i) == data)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ semester2/2.2/2.2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+ namespace Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Table = new HashTable();
+            string choose = "-1";
+            while (choose != "0")
+            {
+                Console.WriteLine();
+                Console.WriteLine("Введите 1, чтобы добавить значение в таблицу");
+                Console.WriteLine("Введите 2, чтобы удалить значение из таблицы");
+                Console.WriteLine("Введите 3, чтобы проверить принадлежность значения к таблице");
+                Console.WriteLine("Введите 0, чтобы выйти");
+                choose = Console.ReadLine();
+                switch (choose)
+                {
+                    case "1":
+                        {
+                            Console.WriteLine("Введите значение");
+                            Table.Add(Console.ReadLine());
+                            break;
+                        }
+                    case "2":
+                        {
+                            Console.WriteLine("Введите значение для удаления");
+                            if (!Table.Delete(Console.ReadLine()))
+                            {
+                                Console.WriteLine("Ошибка ! Проверьте правильность входных данных");
+                            }
+                            break;
+                        }
+                    case "3":
+                        {
+                            Console.WriteLine("Введите значение для проверки");
+                            if (Table.Exists(Console.ReadLine()))
+                            {
+                                Console.WriteLine("Значение есть в таблице");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Значения нет в таблице");
+                            }
+                            break;
+                        }
+                }
+            }
+            Table.Clear();
+        }
+    }
+} 
