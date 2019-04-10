b51d68c1d2d5894250b9de3de02e29db66e1229a
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2019-04-10 21:00:10 +0300

    [ci skip] Тест codacy

--- semester2/2.2/2.2/HashTable.cs
+++ semester2/2.2/2.2/HashTable.cs
@@ -15,7 +15,6 @@ namespace Table
             InitializeTable(myTable, MAX);
         }
 
-
         private void InitializeTable(List[] table, int size)
         {
             for (int i = 0; i < size; ++i)
