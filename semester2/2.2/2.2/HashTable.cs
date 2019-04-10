8cdc1e624eda8f046d0dbfb2a52f210c6ed22240
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2019-04-10 19:58:01 +0300

    Добавил ресайз хеш-таблицу

--- semester2/2.2/2.2/HashTable.cs
+++ semester2/2.2/2.2/HashTable.cs
@@ -1,29 +1,59 @@
 using System;
+using LinkedList;
 
 namespace Table
 {
     public class HashTable
     {
-        private const int MAX = 100;
-        private LinkedList.List[] MyTable = new LinkedList.List[MAX];
+        private int MAX = 100;
+        private List[] myTable;
+        private int filledCells = 0;
 
         public HashTable()
         {
-            for (int i = 0; i < MAX; ++i)
+            myTable = new List[MAX];
+            InitializeTable(myTable, MAX);
+        }
+
+        private void InitializeTable(List[] table, int size)
+        {
+            for (int i = 0; i < size; ++i)
             {
-                MyTable[i] = new LinkedList.List();
+                table[i] = new List();
             }
         }
 
-        private int HashCode(string data)
+        private void Resize()
         {
-            return Math.Abs(data.GetHashCode()) % MAX;
+            MAX += 100;
+            List[] temp = new List[MAX];
+            InitializeTable(temp, MAX);
+            foreach (List list in myTable)
+            {
+                for (int i = 1; i <= list.GetSize(); ++i)
+                {
+                    int hash = HashCode(list.GetDataOn(i));
+                    temp[hash].Add(list.GetDataOn(i), temp[hash].GetSize() + 1);
+                }
+            }
+            myTable = temp;
         }
+
+        private int HashCode(string data)
+            => Math.Abs(data.GetHashCode()) % MAX;
 
         public void Add(string data)
         {
+            if (filledCells / MAX > 0.72)
+            {
+                Resize();
+            }
             int hash = HashCode(data);
-            MyTable[hash].Add(data, MyTable[hash].GetSize() + 1);
+            if (myTable[hash].GetSize() == 0)
+            {
+                ++filledCells;
+            }
+            myTable[hash].Add(data, myTable[hash].GetSize() + 1);
         }
 
         public bool Delete(string data)
@@ -34,29 +64,27 @@ namespace Table
             {
                 return false;
             }
-            MyTable[hash].Delete(position);
+            myTable[hash].Delete(position);
             return true;
         }
 
         public bool Exists(string data)
-        {
-            return (GetPosition(data, HashCode(data)) != -1);
-        }
+            => (GetPosition(data, HashCode(data)) != -1);
 
         public void Clear()
         {
             for (int i = 0; i < MAX; ++i)
             {
-                MyTable[i] = null;
+                myTable[i] = null;
             }
         }
 
         private int GetPosition(string data, int hash)
         {
-            int size = MyTable[hash].GetSize();
+            int size = myTable[hash].GetSize();
             for (int i = 1; i <= size; ++i)
             {
-                if (MyTable[hash].GetDataOn(i) == data)
+                if (myTable[hash].GetDataOn(i) == data)
                 {
                     return i;
                 }
