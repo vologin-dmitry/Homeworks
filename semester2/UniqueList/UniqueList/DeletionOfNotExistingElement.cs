dbbcf8a38ce210d250ab5d274f530ec4b6dd3eae
vologin-dmitry <43678316+vologin-dmitry@users.noreply.github.com>
2019-04-23 06:28:42 +0300

    Написал UniqueList

---
+++ semester2/UniqueList/UniqueList/DeletionOfNotExistingElement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UniqueList
+{
+    /// <summary>
+    /// Exception that throws, when you try to delete an element, which does not exists in the list
+    /// </summary>
+    [Serializable]
+    public class DeletionOfNotExistingElement : InvalidOperationException
+    {
+        public DeletionOfNotExistingElement() { }
+        public DeletionOfNotExistingElement(string message) : base(message) { }
+        public DeletionOfNotExistingElement(string message, Exception inner)
+        : base(message, inner) { }
+        protected DeletionOfNotExistingElement(
+        System.Runtime.Serialization.SerializationInfo info,
+        System.Runtime.Serialization.StreamingContext context)
+        : base(info, context) { }
+    }
+}
---
+++ semester2/UniqueList/UniqueList/UniqueList.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace UniqueList
+{
+    /// <summary>
+    /// List, which throws an exception, when you try to add an element, 
+    /// which already exists in list or delete element, which does not exist in list
+    /// </summary>
+    public class UniqueList : LinkedList.List
+    {
+        /// <summary>
+        /// Adds an element to list. If element, you want to add already exists in this list, throws an exception
+        /// </summary>
+        /// <param name="data">Value, you want to add</param>
+        /// <param name="position">The position to which you want to put the value</param>
+        /// <returns>True if the addition was successful, false if the value was not added</returns>
+        public override bool Add(string data, int position)
+        {
+            if (GetPosition(data) != -1)
+            {
+                throw new AddingExistingElementException();
+            }
+            return base.Add(data, position);
+        }
+
+        /// <summary>
+        /// Removes an element from list. If element, you want to remove does not exists in this list, throws an exception
+        /// </summary>
+        /// <param name="position">The position of the element, you want to remove</param>
+        /// <returns>True if the deletion was successful, false if the value was not deleted</returns>
+        public override bool Delete(int position)
+        {
+            if (!base.Delete(position))
+            {
+                throw new DeletionOfNotExistingElement();
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///Changes the string in the specified position, throws an exception if the value to which you want to change already exists
+        /// </summary>
+        /// <param name="data">The string to which you want to change the string at a given position</param>
+        /// <param name="position">Position of a string, you want to change</param>
+        /// <returns>True, if operation was successful, false otherwise</returns>
+        public override bool SetDataOn(string data, int position)
+        {
+            int alreadyExistsOn = GetPosition(data);
+            if (alreadyExistsOn != position && alreadyExistsOn != -1)
+            {
+                throw new AddingExistingElementException();
+            }
+            return base.SetDataOn(data, position);
+        }
+    }
+}
